f49a44b5b24418091fb6b825f2c68ef77634cea1
Simran Brucherseifer <s.brucherseifer@axiell.com>
2016-03-03 11:26:56 +0100

    Promisify, but no luck so far to make it run in a series...

--- tagCounts.js
+++ tagCounts.js
@@ -15,9 +15,10 @@ for (let entry of process.argv.entries()) {
     let index = entry[0];
     let filePath = entry[1];
     if (index < 2) continue;
-    console.log(`${index-1}. ${path.basename(filePath)}`);
 
-    processFile(filePath, outFile);
+    processFile(filePath, outFile).then(function() {
+        console.log(`${index-1}. ${path.basename(filePath)}`);
+    });
 }
 
 console.log();
@@ -25,47 +26,49 @@ console.timeEnd("Total time");
 console.log();
 
 function processFile(filePath, outFile) {
-    let inFile = fs.createReadStream(filePath).pipe(new bomstrip());
+    return new Promise(function() {
+        let inFile = fs.createReadStream(filePath).pipe(new bomstrip());
 
-    let lineReader = readline.createInterface({
-        input: inFile
-    });
-    let tagCounts = {};
-    let recordCount = 0;
-    
-    lineReader.on("line", function(line) {
-        let tag = line.slice(0, 2);
-        tag.trim();
-        if (!tag) {
-            return;
-        }
-        else if (tag == "**") {
-            recordCount++;
-        } else {
-            tagCounts[tag] = tagCounts[tag] + 1 || 1;
-        }
-    });
-    
-    // must wait for close event to access the final states of the variables
-    lineReader.on("close", function() {
-        let basename = path.basename(filePath);
-        outFile.write(basename + "\n");
-        outFile.write("-".repeat(basename.length) + "\n");
-        outFile.write(`Path: ${path.resolve(filePath)}\n`)
-        outFile.write(`Total records: ${recordCount}\n\n`);
-        
-        let tagCountsArray = [];
-        for (let elem in tagCounts) {
-            tagCountsArray.push([tagCounts[elem], elem]);
-        }
-        tagCountsArray.sort(function(a, b) {
-            return b[0] - a[0]
+        let lineReader = readline.createInterface({
+            input: inFile
         });
-        for (let elem of tagCountsArray) {
-            let count = elem[0];
-            let tag = elem[1];
-            outFile.write(`${" ".repeat(8 - count.toString().length)}${count} ${tag}\n`);
-        }
-        outFile.write("\n\n");
+        let tagCounts = {};
+        let recordCount = 0;
+        
+        lineReader.on("line", function(line) {
+            let tag = line.slice(0, 2);
+            tag.trim();
+            if (!tag) {
+                return;
+            }
+            else if (tag == "**") {
+                recordCount++;
+            } else {
+                tagCounts[tag] = tagCounts[tag] + 1 || 1;
+            }
+        });
+        
+        // must wait for close event to access the final states of the variables
+        lineReader.on("close", function() {
+            let basename = path.basename(filePath);
+            outFile.write(basename + "\r\n");
+            outFile.write("-".repeat(basename.length) + "\r\n");
+            outFile.write(`Path: ${path.resolve(filePath)}\r\n`)
+            outFile.write(`Total records: ${recordCount}\r\n\r\n`);
+            
+            let tagCountsArray = [];
+            for (let elem in tagCounts) {
+                tagCountsArray.push([tagCounts[elem], elem]);
+            }
+            tagCountsArray.sort(function(a, b) {
+                return b[0] - a[0]
+            });
+            for (let elem of tagCountsArray) {
+                let count = elem[0];
+                let tag = elem[1];
+                outFile.write(`${" ".repeat(8 - count.toString().length)}${count} ${tag}\r\n`);
+            }
+            outFile.write("\r\n\r\n");
+        });
     });
 }
